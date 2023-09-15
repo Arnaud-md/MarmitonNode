62102342b462239ffb3b89ea1d33e9b36c1ab1fa
Arnaud Laforgue <arnaud.laforgue@gmail.com>
2023-09-15 11:53:12 +0200

    commit avant l'ajout de CORS

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
-import express from "express";
+import express, { Request } from "express";
 import 'dotenv/config';
-import {Recipe} from './seqconfig'
-
+import {Recipe} from './seqconfig';
+import bodyParser from "body-parser";
+import { IntegerDataType } from "sequelize";
 const app = express();
+app.use(bodyParser.json());
 const port = process.env.PORT ? parseInt(process.env.PORT as string) : 3030
+
+interface IMaRequetBody {
+    nom: string,
+    image: string,
+    duree: IntegerDataType,
+    note: IntegerDataType
+  }
+  
+  app.post("/send-recipe", (req, res) => {
+    const nom = req.body.nom;
+    const image = req.body.image;
+    const duree = req.body.duree;
+    const note = req.body.note;
+    const maRecette = { nom, image ,duree, note }
+    console.log(maRecette);
+    Recipe.create(maRecette);
+    res.json(maRecette);
+  })
 
 app.get('/random-between/:min/:max', (req, res) => {
     const min = parseInt(req.params.min)
@@ -18,13 +38,14 @@ app.post('/saveRecipe/:nom/:image/:duree/:note', (req, res) => {
     const image = req.params.image;
     const duree = parseInt(req.params.duree);
     const note = parseInt(req.params.note);
-    Recipe.create({
+    const myRecipe = {
         nom: nom,
         image : image,
         duree : duree,
         note : note
-    });
-    res.send(nom+image+duree+note)
+    };
+    Recipe.create(myRecipe);
+    res.send(myRecipe);
 })
 
 app.get('/findAll', async(req, res) => {
